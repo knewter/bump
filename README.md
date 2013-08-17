c40b12acb1ad2866076b36a261187466724a70b7
Josh Adams <josh@isotope11.com>
2013-08-16 20:42:03 -0500

    Some updates to get tests running

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Just working on my unit testing chops in erlang.  Nothing to see here, move alon
 To run the tests:
 
 ```shell
-rebar test
+rebar eunit
 ```
--- src/bump_app.erl
+++ src/bump_app.erl
@@ -5,6 +5,10 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
@@ -14,3 +18,9 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
+
+-ifdef(TEST).
+simple_test() ->
+    ok = application:start(bump),
+    ?assertNot(undefined == whereis(bump_sup)).
+-endif.
